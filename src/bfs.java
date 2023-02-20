c2b51a616be0a19815259a71184c6f2ae4cd7aaf
joanne.seymour <1619878217@qq.com>
2023-02-20 20:00:12 +0800

    added to bfs.java

--- src/bfs.java
+++ src/bfs.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 public class bfs {
     static Node goal = Problem.goal;
     static Problem problem = new Problem(BusRoutes.home, 0, goal);
-    Node initialState = BusRoutes.home;
-    static Node nodeBeingChecked = new Node(BusRoutes.home.place);
-    ArrayList<Node> frontier = new ArrayList<Node>();
-
+    static Node initialState = new Node(BusRoutes.home.place);
+    static ArrayList<Node> frontier = new ArrayList<Node>();
+    static Node nodeBeingChecked;
+    static ArrayList<Node> explored = new ArrayList<Node>();
 
 
     public static void main(String[] args) {
@@ -16,6 +16,14 @@ public class bfs {
         } else {
             System.out.println("Goal not reached yet");
         }
-        ;
+        if (frontier.size()>0) {
+            // remove first or last from frontier?
+            nodeBeingChecked = frontier.remove(frontier.size());
+            explored.add(nodeBeingChecked);
+            // for each action in problem.ACTIONS(node.STATE) do
+
+        } else {
+            System.out.println("Fail");
+        }
     }
 }
